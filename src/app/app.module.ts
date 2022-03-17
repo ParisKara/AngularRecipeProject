ad534df01faadd3df0226929fd519c2dee3b6f86
ParisKara <pariskarampatzakis@gmail.com>
2022-03-17 18:32:27 +0200

    Added the FooterComponent. Dropdown is not working.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { DropdownDirective } from './shared/dropdown.directive';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    DropdownDirective,
+    FooterComponent
   ],
   imports: [
     BrowserModule
